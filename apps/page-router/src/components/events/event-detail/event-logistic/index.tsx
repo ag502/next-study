bae4ff2f707d5fb2dfc8e028aac5d2e5dc706187
ag502 <ag502@naver.com>
2024-08-04 21:52:59 +0900

    feat: event datail 페이지 추가

---
+++ apps/page-router/src/components/events/event-detail/event-logistic/index.tsx
@@ -0,0 +1,35 @@
+import AddressIcon from "@/components/icons/address-icon";
+import DateIcon from "@/components/icons/date-icon";
+
+import LogisticsItem from "../logistic-item";
+
+import classes from "./event-logistics.module.css";
+
+function EventLogistics(props: any) {
+  const { date, address, image, imageAlt } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const addressText = address.replace(", ", "\n");
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <img alt={imageAlt} src={`/${image}`} />
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
--- apps/page-router/src/pages/events/[event-id].tsx
+++ apps/page-router/src/pages/events/[event-id].tsx
@@ -1,5 +1,28 @@
+import { useRouter } from "next/router";
+
+import EventContent from "@/components/events/event-detail/event-content";
+import EventLogistics from "@/components/events/event-detail/event-logistic";
+import EventSummary from "@/components/events/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+
 export default function EventDetailPage() {
-    return (
-        <div>This is Event Detail Page</div>
-    )
+  const { query } = useRouter();
+
+  const eventId = query["event-id"];
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics address={event.location} date={event.date} image={event.image} imageAlt={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
